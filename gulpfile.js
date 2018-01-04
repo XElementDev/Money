82ade01889dedcbf8a411d82f2ec73923bd89d9c
IanStorm <IanStorm28@outlook.com>
2018-01-04 22:38:31 +0100

    introduced "tsoa" as it will be needed soon

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var del = require("del");
+var execSync = require("child_process").execSync;//Node.js
 var gulp = require("gulp");
 var path = require("path");//Node.js
 var sequence = require("gulp-sequence");
 var sourcemaps = require("gulp-sourcemaps");
+var through = require("through2");
 var ts = require("gulp-typescript");
 
 
@@ -17,9 +19,11 @@ var buildTask = "build";
 var cleanTask = "clean";
 var rebuildTask = "rebuild";
 
+var internalTsoaTask = "_tsoa";
 var internalTypescriptTask = "_ts";
 
 gulp.task(buildTask, sequence(
+	internalTsoaTask, 
 	internalTypescriptTask
 ));
 
@@ -31,6 +35,19 @@ gulp.task(rebuildTask, sequence(
 	cleanTask, 
 	buildTask
 ));
+
+gulp.task(internalTsoaTask, function() {
+	var globs = path.join(srcFolderName, allFolders, "tsoa.json");
+	var src$ = gulp.src(globs, {read: false})
+		.pipe(through.obj(function(file, enc, cb) { // see https://stackoverflow.com/questions/27923523/how-can-i-write-a-simple-gulp-pipe-function
+			var folderPath = path.dirname(file.path);
+			execSync("tsoa swagger", {cwd: folderPath});
+			execSync("tsoa routes", {cwd: folderPath});
+			cb(null, file);
+		}))
+	;
+	return src$;
+});
 
 gulp.task(internalTypescriptTask, function() {
 	var writeOptions = {
