ec438ec91af8661bb95b80160bbc676560a76092
IanStorm <IanStorm28@outlook.com>
2018-01-07 13:09:34 +0100

    [HTML client] new "add new asset" page behavior:
    * showing the correct title header (i.e. only one)
    * showing the today's date as the default purchase date
    * showing a default price
    * resetting the comment

--- src/Clients/HTML/assets/new/index.ts
+++ src/Clients/HTML/assets/new/index.ts
@@ -4,13 +4,57 @@
 class NewAssetViewModel {
 
 	public constructor() {
+		this.attachBehavior();
 		this.setDefaultValues();
 	}
 
 
-	private setDefaultValues(): void {
-		$("#asset-type-expense").prop("checked", true);
+	private attachBehavior(): void {
+		$("#asset-type-earning").on("click", () => {
+			$(NewAssetViewModel.TITLE_HEADER_EXPENSE).hide();
+			$(NewAssetViewModel.TITLE_HEADER_EARNING).show();
+		});
+
+		$(NewAssetViewModel.RADIO_BUTTON_EXPENSE).on("click", () => {
+			$(NewAssetViewModel.TITLE_HEADER_EARNING).hide();
+			$(NewAssetViewModel.TITLE_HEADER_EXPENSE).show();
+		});
 	}
+
+
+	private static padLeft(target: any, 
+	                       totalWidth: number, 
+	                       paddingStr: string): string {
+		let str = `${target}`;
+		while(str.length < totalWidth) {
+			str = paddingStr + str;
+		}
+		return str;
+	}
+
+
+	private setDefaultValues(): void {
+		$(NewAssetViewModel.RADIO_BUTTON_EXPENSE).click();
+
+		const today = new Date();
+		const year = today.getFullYear();
+		const month = today.getMonth()+1;
+		const monthStr = NewAssetViewModel.padLeft(month, 2, "0");
+		const day = today.getDate();
+		const dayStr = NewAssetViewModel.padLeft(day, 2, "0");
+		$("#purchase-date").val(`${year}-${monthStr}-${dayStr}`);
+
+		$("#price").val("0.00");
+
+		$("#comment").val("");
+	}
+
+
+	private static readonly RADIO_BUTTON_EXPENSE = "#asset-type-expense";
+
+	private static readonly TITLE_HEADER_EARNING = ".title-header-earning";
+
+	private static readonly TITLE_HEADER_EXPENSE = ".title-header-expense";
 
 }
 
